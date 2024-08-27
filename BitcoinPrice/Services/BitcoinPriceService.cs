32da35aed701999636b62b0a17f7f40185d75daf
Milad Raeisi <miladsoft@yahoo.com>
2024-08-27 15:47:40 +0400

    Upload the app

---
+++ BitcoinPrice/Services/BitcoinPriceService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class BitcoinPriceService
+{
+    private readonly HttpClient _httpClient;
+
+    public BitcoinPriceService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<decimal> GetBitcoinPriceAsync()
+    {
+        var response = await _httpClient.GetFromJsonAsync<BitcoinPriceClass>("https://mempool.space/api/v1/prices");
+        return response?.USD ?? 0;
+    }
+}
+
+public class BitcoinPriceClass
+{
+    public decimal USD { get; set; }
+}
